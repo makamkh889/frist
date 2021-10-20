e080039e8cb4faf70638bc7011c38b815e445fdc
mariam <makamkh889>
2021-10-20 13:30:34 +0200

    new commit

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Enter two numper\n";
+	long long n, m;
+	cin >> n >> m;
+	cout << n <<" + "<< m<<" = "<<n+m<<"\n";
+	system("pause");
+	return 0;
+}
